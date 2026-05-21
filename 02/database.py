fb9ceff25fd7621db303b989a9e95e52f71db632
HojuneLee <147648712+HojuneLee0106@users.noreply.github.com>
2026-05-21 16:23:48 +0900

    Update database.py

--- 02/database.py
+++ 02/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:hojune0701!@localhost:3306/community_app?charset=utf8mb4"
+DATABASE_URL = "mysql+pymysql://root:비밀번호입력@localhost:3306/community_app?charset=utf8mb4"
 
 engine = create_engine(
     DATABASE_URL,
